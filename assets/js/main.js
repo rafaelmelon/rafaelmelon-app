8adc529b9ff09071d16ba0e4f8c706e843150e82
Rafael Melón <info@rafaelmelon.es>
2017-06-04 21:49:55 +0200

    Update color brand

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $('.countdown').countdown('2017/07/01', function(event) {
-    $(this).html(event.strftime('<h3><span>%w</span> semanas, <span>%d</span> días y <span>%M:%H:%S</span></h3>'));
+    $(this).html(event.strftime('<h3 class="h3"><span>%w</span>W · <span>%d</span>D · <span>%M:%H:%S</span>H</h3>'));
   });
 
   $('#web-contact').validator().on('submit', function (e) {
