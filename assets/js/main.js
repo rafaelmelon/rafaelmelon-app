23f25bb9bdcc155528d9b942ba78b6fcd0e3bcf3
Rafael Melón <info@rafaelmelon.es>
2017-09-03 19:22:46 +0200

    Push date

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $('.countdown').countdown('2017/09/01', function(event) {
+  $('.countdown').countdown('2017/12/31', function(event) {
     $(this).html(event.strftime('<h3 class="h3"><span>%w</span>W · <span>%d</span>D · <span>%M:%H:%S</span>H</h3>'));
   });
 
