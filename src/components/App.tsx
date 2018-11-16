40b4a7bc06ed93edaa9c718a008b87ed4154de5d
Rafael Melón <info@rafaelmelon.es>
2018-11-17 00:56:06 +0100

    feat(lint): tslint/prettier config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,17 @@
-import * as React from "react";
+import * as React from 'react';
 
-const reactLogo = require("./../assets/img/react_logo.svg");
+const reactLogo = require('./../assets/img/react_logo.svg');
 
-export interface IProps {
-}
+export interface IProps {}
 
 export default class App extends React.Component<IProps, undefined> {
-    render() {
-        return (
-            <div>
-                <h1>Hello World!</h1>
-                <p>Foo to the barz</p>
-                <img src={reactLogo} height="480"/>
-            </div>
-        );
-    }
+  public render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <p>Foo to the barz</p>
+        <img src={reactLogo} height="480" />
+      </div>
+    );
+  }
 }
