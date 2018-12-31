8e2404608a2dd967f6aa1ebda369fd7b45ccc1c0
Rafael Melón <info@rafaelmelon.es>
2018-12-31 19:39:49 +0100

    fix(express): prevent refresh route

--- server/express.js
+++ server/express.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const portNumber = process.env.PORT || 8080;
 const sourceDir = 'dist';
 
 app.use(express.static(sourceDir));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://localhost:${portNumber}`);
